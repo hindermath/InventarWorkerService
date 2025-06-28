eb30738dc8ab523076783bef00fac24ac9f97a9f
Thorsten Hindermann <hindermath@googlemail.com>
2025-06-28 15:22:12 +0200

    Add CpuInfo model to InventarWorkerService for CPU usage and system architecture representation

---
+++ InventarWorkerService/Models/CpuInfo.cs
@@ -0,0 +1,9 @@
+namespace InventarWorkerService.Models;
+
+public record CpuInfo
+{
+    public int ProcessorCount { get; set; }
+    public string ProcessorName { get; set; } = string.Empty;
+    public string Architecture { get; set; } = string.Empty;
+    public double CurrentUsage { get; set; }
+}
