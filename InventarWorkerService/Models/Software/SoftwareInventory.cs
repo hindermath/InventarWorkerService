10acbf9843670d0f8d161a274ee6870fdf4357ae
Thorsten Hindermann <hindermath@googlemail.com>
2025-06-28 19:52:28 +0200

    Add `SoftwareInventory` model to `InventarWorkerService` for software inventory management

---
+++ InventarWorkerService/Models/Software/SoftwareInventory.cs
@@ -0,0 +1,12 @@
+namespace InventarWorkerService.Models;
+
+public record SoftwareInventory
+{
+    public DateTime CollectionTime { get; set; } = DateTime.Now;
+    public List<SoftwareInfo> InstalledSoftware { get; set; } = new();
+    public List<ProcessInfo> RunningProcesses { get; set; } = new();
+    public List<ServiceInfo> WindowsServices { get; set; } = new();
+    public List<string> StartupPrograms { get; set; } = new();
+    public List<string> EnvironmentVariables { get; set; } = new();
+    public RuntimeInfo Runtime { get; set; } = new();
+}
