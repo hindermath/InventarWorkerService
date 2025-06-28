e893add9e2fbb4ed024ade3eed6444d98e8a97fc
Thorsten Hindermann <hindermath@googlemail.com>
2025-06-28 21:40:35 +0200

    Switch to `WebApplication` for hosting and add REST API support with CORS, Swagger, and controllers setup.

--- InventarWorkerService/Program.cs
+++ InventarWorkerService/Program.cs
@@ -1,13 +1,42 @@
 using InventarWorkerService;
 using InventarWorkerService.Services.Hardware;
 using InventarWorkerService.Services.Software;
+using Microsoft.AspNetCore.Builder;
 
-var builder = Host.CreateApplicationBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Services registrieren
 builder.Services.AddSingleton<HardwareInventoryService>();
 builder.Services.AddSingleton<SoftwareInventoryService>();
 builder.Services.AddHostedService<Worker>();
 
-var host = builder.Build();
-host.Run();
+// REST API Services hinzufügen
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// CORS konfigurieren (optional)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Development Environment konfigurieren
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
