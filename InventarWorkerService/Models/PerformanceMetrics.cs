241404966a069840a25d6e280d02f210007d6922
Thorsten Hindermann <hindermath@googlemail.com>
2025-06-28 10:31:57 +0200

    Extend PerformanceMetrics model with CpuUsage, MemoryUsage, ThreadCount, and Timestamp properties

--- InventarWorkerService/Models/PerformanceMetrics.cs
+++ InventarWorkerService/Models/PerformanceMetrics.cs
@@ -1,3 +1,9 @@
 namespace InventarWorkerService.Models;
 
-public record PerformanceMetrics();
+public record PerformanceMetrics
+{
+    public double CpuUsage { get; init; }
+    public long MemoryUsage { get; init; }
+    public int ThreadCount { get; init; }
+    public DateTime Timestamp { get; init; }
+}
