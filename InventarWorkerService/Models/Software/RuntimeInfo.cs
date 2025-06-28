e39636238ada190b1722ae25bbe2a4570ad151fc
Thorsten Hindermann <hindermath@googlemail.com>
2025-06-28 19:51:45 +0200

    Add `RuntimeInfo` model to `InventarWorkerService` for runtime environment details

---
+++ InventarWorkerService/Models/Software/RuntimeInfo.cs
@@ -0,0 +1,9 @@
+namespace InventarWorkerService.Models;
+
+public record RuntimeInfo
+{
+    public string DotNetVersion { get; set; } = string.Empty;
+    public List<string> InstalledFrameworks { get; set; } = new();
+    public string PowerShellVersion { get; set; } = string.Empty;
+    public List<string> AvailableModules { get; set; } = new();
+}
