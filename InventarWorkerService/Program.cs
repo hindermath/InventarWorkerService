8b31780e77900691ae2948ddbfa16b3ee6fe428d
Thorsten Hindermann <hindermath@googlemail.com>
2025-06-28 23:36:58 +0200

    Configure JSON serialization options in `Program.cs` with camelCase formatting, indented output, and support for named floating-point literals.

--- InventarWorkerService/Program.cs
+++ InventarWorkerService/Program.cs
@@ -1,7 +1,10 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using InventarWorkerService;
 using InventarWorkerService.Services.Hardware;
 using InventarWorkerService.Services.Software;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +17,14 @@ builder.Services.AddHostedService<Worker>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<JsonOptions>(options =>
+{
+    // JSON-Serialisierung konfigurieren
+    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    options.JsonSerializerOptions.WriteIndented = true;
+    options.JsonSerializerOptions.NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals;
+});
 
 // CORS konfigurieren (optional)
 builder.Services.AddCors(options =>
