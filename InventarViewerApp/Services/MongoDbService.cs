a1d7a6339df0bfc7ef649f766d0bab6dbab8d632
Thorsten Hindermann <hindermath@googlemail.com>
2025-07-19 17:52:08 +0200

    Enhance `MongoDbService` with database initialization and save functionality
    - Added `_mongoClient` and `_mongoDatabase` for managing MongoDB connections.
    - Renamed `InitializeDatabase` to `InitializeMongoDatabase` for clarity.
    - Implemented `SaveSoftwareInventoryAsync` to save software inventory as BSON documents.

--- InventarViewerApp/Services/MongoDbService.cs
+++ InventarViewerApp/Services/MongoDbService.cs
@@ -1,24 +1,30 @@
 using InventarViewerApp.Models.Software;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace InventarViewerApp.Services;
 
 public class MongoDbService
 {
     private readonly string _connectionString;
+    private readonly IMongoClient _mongoClient;
+    private IMongoDatabase _mongoDatabase;
 
     public MongoDbService(string connectionString)
     {
         _connectionString = connectionString;
+        _mongoClient = new MongoClient(_connectionString);
     }
 
-    public void InitializeDatabase()
+    public void InitializeMongoDatabase()
     {
-        
+        _mongoDatabase = _mongoClient.GetDatabase("Inventory");
     }
 
     public async Task SaveSoftwareInventoryAsync(int machineId, SoftwareInventory software)
     {
-        
+        var collection = _mongoDatabase.GetCollection<BsonDocument>($"{machineId}");
+        await collection.InsertOneAsync(software.ToBsonDocument());
     }
 
 }
