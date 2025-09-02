16bf293b7ebf779642ca88699996a7f466c6d170
Thorsten Hindermann <hindermath@googlemail.com>
2025-09-02 08:11:43 +0200

    INV-4 FQDN und Port der Client REST API
    Add XML documentation to `Initialize` class and `Services` method for improved clarity and maintainability

--- InventarWorkerCommon/Services/Common/Initialize.cs
+++ InventarWorkerCommon/Services/Common/Initialize.cs
@@ -3,8 +3,20 @@ using InventarWorkerCommon.Services.Database;
 
 namespace InventarWorkerCommon.Services.Common;
 
+/// <summary>
+/// Provides a static method to initialize and configure essential services used in the application.
+/// This includes API service, SQLite database service, and MongoDB service.
+/// </summary>
 public static class Initialize
 {
+    /// <summary>
+    /// Initializes and configures essential services including API service, SQLite database service,
+    /// and MongoDB service with optional parameters for hostnames and port configurations.
+    /// </summary>
+    /// <param name="clientApiFqdn">The fully qualified domain name (FQDN) or hostname of the client API. Defaults to "localhost".</param>
+    /// <param name="clientApiPort">The port number used for the client API. Defaults to "5000".</param>
+    /// <param name="mongoDbFqdn">The fully qualified domain name (FQDN) or hostname of the MongoDB service. Defaults to "localhost".</param>
+    /// <returns>A tuple containing the initialized services: <c>ApiService</c>, <c>SqliteDbService</c>, and <c>MongoDbService</c>.</returns>
     public static (ApiService apiService, SqliteDbService dbService, MongoDbService mongoDbService) Services(
         string clientApiFqdn = "localhost",
         string clientApiPort = "5000",
