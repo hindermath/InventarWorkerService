def384fcef3abbfde51cd898081988baebec7da9
Thorsten Hindermann <hindermath@googlemail.com>
2025-09-06 17:52:39 +0200

    Add `BasePath` class to resolve platform-specific base directory paths
    - Includes logic to determine the base path for Windows, MacOS, and other operating systems.
    - Provides default paths: common application data for Windows, shared user directory for MacOS, and `/usr/share` for others.
    - Adds XML documentation for improved code clarity.

---
+++ InventarWorkerCommon/Services/Paths/BasePath.cs
@@ -0,0 +1,26 @@
+namespace InventarWorkerCommon.Services.Paths;
+
+/// <summary>
+/// Provides functionality to retrieve the base path of the system
+/// based on the operating system.
+/// </summary>
+public class BasePath
+{
+    /// <summary>
+    /// Retrieves the base path of the system depending on the operating system.
+    /// </summary>
+    /// <returns>
+    /// Returns the base directory path as a string.
+    /// For Windows, it is the common application data directory.
+    /// For MacOS, it is the shared users directory.
+    /// For other operating systems, it defaults to '/usr/share'.
+    /// </returns>
+    public static string GetBasePath()
+    {
+        return OperatingSystem.IsWindows()
+            ?
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData))
+            :
+            OperatingSystem.IsMacOS() ? "/Users/Shared" : "/usr/share";
+    }
+}
