29827a1c77f0ad622b9187a6eda7e5e233ad3fc1
Thorsten Hindermann <hindermath@googlemail.com>
2025-09-06 21:55:07 +0200

    Add methods to manage service status directory existence and creation
    - Introduce `CreateServiceStatusPath` to create a directory at the service status path.
    - Add `ExistsServiceStatusPath` to validate the existence of the service status directory.
    - Include XML documentation for both methods to improve code clarity.

--- InventarWorkerCommon/Services/Paths/ServicePath.cs
+++ InventarWorkerCommon/Services/Paths/ServicePath.cs
@@ -17,4 +17,31 @@ public static class ServicePath
         return System.IO.Path.Combine(BasePath.GetBasePath(),
             Environment.GetEnvironmentVariable("SERVICESTATUSDIRECTORY") ?? "InventarWorkerService");
     }
+
+    /// <summary>
+    /// Creates a directory at the specified service status path if it does not already exist.
+    /// Validates the creation by checking if the directory exists after the attempt.
+    /// </summary>
+    /// <param name="serviceStatusPath">The path where the service status directory is to be created.</param>
+    /// <returns>
+    /// A boolean value indicating whether the directory exists after the creation attempt.
+    /// Returns true if the directory exists, false otherwise.
+    /// </returns>
+    public static DirectoryInfo CreateServiceStatusPath(string serviceStatusPath)
+    {
+        return Directory.CreateDirectory(serviceStatusPath);
+    }
+
+    /// <summary>
+    /// Checks whether the specified service status path exists in the file system.
+    /// </summary>
+    /// <param name="serviceStatusPath">The path to the service status directory to check for existence.</param>
+    /// <returns>
+    /// A boolean value indicating whether the specified path exists in the file system.
+    /// Returns true if the path exists, false otherwise.
+    /// </returns>
+    public static bool ExistsServiceStatusPath(string serviceStatusPath)
+    {
+        return Directory.Exists(serviceStatusPath);   
+    }
 }
