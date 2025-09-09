475bdb26ea188a968b3a5e62f6700a58a6f9cb97
Thorsten Hindermann <thorsten.hindermann@gwdg.de>
2025-09-09 13:11:48 +0200

    Refine XML documentation in `ServicePath.cs` for improved clarity and formatting.

--- InventarWorkerCommon/Services/Paths/ServicePath.cs
+++ InventarWorkerCommon/Services/Paths/ServicePath.cs
@@ -7,7 +7,8 @@ public static class ServicePath
 {
     /// <summary>
     /// Retrieves the platform-specific path used to store or access service status information.
-    /// Combines the base directory with a service status directory environment variable or defaults to a predefined name if the variable is not set.
+    /// Combines the base directory with a service status directory environment variable or defaults to a predefined
+    /// name if the variable is not set.
     /// </summary>
     /// <returns>
     /// A string representing the computed service status path.
