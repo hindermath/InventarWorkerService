af68b3140dfe629c7dc3e6881845046d900e8bc6
Thorsten Hindermann <thorsten.hindermann@gwdg.de>
2025-09-09 13:12:59 +0200

    Add XML documentation for `SystemInfo` to describe its purpose and included details

--- InventarWorkerCommon/Models/Service/SystemInfo.cs
+++ InventarWorkerCommon/Models/Service/SystemInfo.cs
@@ -1,5 +1,9 @@
 namespace InventarWorkerCommon.Models.Service;
 
+/// <summary>
+/// Represents system-related information, including details about the machine, user, domain,
+/// uptime, platform, and architecture.
+/// </summary>
 public record SystemInfo
 {
     public string MachineName { get; set; } = string.Empty;
