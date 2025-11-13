5f1b6e2d453b4d02d5e3ccb77f5306f26516dc64
Thorsten Hindermann <hindermath@googlemail.com>
2025-11-13 23:46:40 +0100

    Add `Query.js` for Inventory database queries
    - Introduce `Query.js` with MongoDB queries to analyze `InstalledSoftware` and `RunningProcesses`.
    - Implement aggregate pipelines for software version filtering, process details formatting, and sorting for improved readability.
    - Enhance data extraction with regex-based matching and projection for detailed software and process insights.

---
+++ .idea/.idea.InventarWorkerService/.idea/queries/Query.js
@@ -0,0 +1,59 @@
+// Select Inventory database
+db.getCollectionNames();
+
+db.getCollection('8').find();
+db.getCollection('8').find({ "InstalledSoftware.Name":  "Microsoft Edge" });
+// Unpack InstalledSoftware-Array for better readability
+db.getCollection('8').aggregate([
+    { $unwind: "$InstalledSoftware" },
+    { $project: { "InstalledSoftware": 1 } }
+]);
+
+// Alternative: Regex-basierte Suche für Versionen
+db.getCollection('8').aggregate([
+    { $unwind: "$InstalledSoftware" },
+    {
+        $match: {
+            "InstalledSoftware.Version": {
+                $regex: "^([1-9]|[1-9]\\d+)"
+            }
+        }
+    },
+    {
+        $project: {
+            "SoftwareName": "$InstalledSoftware.Name",
+            "Version": "$InstalledSoftware.Version",
+            "Publisher": "$InstalledSoftware.Publisher"
+        }
+    },
+    { $sort: { "SoftwareName": -1, "Version": 1 } }
+]);
+db.getCollection("8").aggregate(
+    {$unwind: "$InstalledSoftware"},
+    {$project: {"SoftwareName": "$InstalledSoftware.Name", "Version": "$InstalledSoftware.Version", _id: 0}},
+    {$sort: {"Version": -1, "SoftwareName": -1} }
+);
+
+db.getCollection("8").aggregate([
+    { $unwind: "$RunningProcesses" },
+    {
+        $project: {
+            "ProcessName": "$RunningProcesses.ProcessName",
+            "ProcessId": "$RunningProcesses.ProcessId",
+            "StartTimeOnly": {
+                $dateToString: {
+                    format: "%H:%M:%S",
+                    date: "$RunningProcesses.StartTime"
+                }
+            },
+            "StartDateOnly": {
+                $dateToString: {
+                    format: "%d.%m.%Y",
+                    date: "$RunningProcesses.StartTime"
+                }
+            },
+            _id: 0
+        }
+    },
+    { $sort: { "StartTimeOnly": -1 } }
+]);
