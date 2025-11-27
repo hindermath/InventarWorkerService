f67d87debd338c9479827a02a90e91722d27e195
Thorsten Hindermann <hindermath@googlemail.com>
2025-11-27 08:32:07 +0100

    Add `SettingsReader` and `SettingsWriter` classes to handle application configuration reading and writing

---
+++ InventarWorkerCommon/Services/Settings/SettingsReader.cs
@@ -0,0 +1,11 @@
+namespace InventarWorkerCommon.Services.Settings;
+
+/// <summary>
+/// Provides functionality to read configuration settings from various sources.
+/// This class is designed to simplify the retrieval and management of settings
+/// across the application.
+/// </summary>
+public class SettingsReader
+{
+
+}
---
+++ InventarWorkerCommon/Services/Settings/SettingsWriter.cs
@@ -0,0 +1,9 @@
+namespace InventarWorkerCommon.Services.Settings;
+
+/// <summary>
+/// Provides functionality to handle the writing of application settings.
+/// </summary>
+public class SettingsWriter
+{
+
+}
