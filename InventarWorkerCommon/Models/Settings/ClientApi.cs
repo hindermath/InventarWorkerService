73828f3e959ae203fcde5bbde3da3bf9672a06d5
Thorsten Hindermann <hindermath@googlemail.com>
2025-11-27 08:30:18 +0100

    Add `ClientApi` settings model to configure Client API FQDN and port

---
+++ InventarWorkerCommon/Models/Settings/ClientApi.cs
@@ -0,0 +1,17 @@
+namespace InventarWorkerCommon.Models.Settings;
+
+/// <summary>
+/// Represents the configuration settings required to connect to the Client API.
+/// </summary>
+public class ClientApi
+{
+    /// <summary>
+    /// Gets the fully qualified domain name (FQDN) of the client API.
+    /// </summary>
+    public string ClientApiFqdn { get; private set; }
+
+    /// <summary>
+    /// Gets the port of the client API.
+    /// </summary>
+    public string ClientApiPort { get; private set; }
+}
