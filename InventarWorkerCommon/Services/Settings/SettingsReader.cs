ece6cc7d2c3d106468dc40458cb0ff803d7ea009
Thorsten Hindermann <hindermath@msn.com>
2025-11-28 17:22:05 +0100

    Added `SettingsReader` class to manage reading and deserialization of configuration settings from a designated status directory.

--- InventarWorkerCommon/Services/Settings/SettingsReader.cs
+++ InventarWorkerCommon/Services/Settings/SettingsReader.cs
@@ -1,3 +1,7 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using InventarWorkerCommon.Services.Paths;
+
 namespace InventarWorkerCommon.Services.Settings;
 
 /// <summary>
@@ -7,5 +11,51 @@ namespace InventarWorkerCommon.Services.Settings;
 /// </summary>
 public class SettingsReader
 {
+    private readonly string _statusDirectory;
+    private readonly JsonSerializerOptions _jsonOptions;
 
+    /// <summary>
+    /// Provides functionality to read configuration settings for the application.
+    /// This class enables reading configuration data from a predefined status directory
+    /// and deserializes it into strongly-typed objects.
+    /// </summary>
+    public SettingsReader(string statusDirectory = "inventar-service")
+    {
+        if (ServicePath.ExistsServiceStatusPath(Path.Combine(ServicePath.GetServiceStatusPath(), statusDirectory)))
+            _statusDirectory = Path.Combine(ServicePath.GetServiceStatusPath(), statusDirectory);
+        _jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals
+        };
+
+    }
+
+    /// <summary>
+    /// Reads the configuration settings from a predefined status directory.
+    /// This method attempts to locate and deserialize the settings stored in a JSON file
+    /// within the specified status directory. If the file doesn't exist or an error occurs
+    /// during the read operation, it returns null.
+    /// </summary>
+    /// <returns>
+    /// An instance of the <see cref="Models.Settings.Settings"/> class containing
+    /// the deserialized configuration settings, or null if the file is not found or cannot be read.
+    /// </returns>
+    public Models.Settings.Settings? ReadSettings()
+    {
+        try
+        {
+            var settingsFile = System.IO.Path.Combine(_statusDirectory, "settings.json");
+            if (!File.Exists(settingsFile)) return null;
+
+            var json = File.ReadAllText(settingsFile);
+            return JsonSerializer.Deserialize<Models.Settings.Settings>(json, _jsonOptions);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Fehler beim Lesen des Status: {ex.Message}");
+            return null;
+        }
+
+    }
 }
