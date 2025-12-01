26eb41e8257d856b5d8bff2e818745f4a750add4
Thorsten Hindermann <hindermath@googlemail.com>
2025-12-01 16:26:42 +0100

    Add `ClientApiUrl` property to `ClientApi` for constructing API URL from FQDN and port

--- InventarWorkerCommon/Models/Settings/ClientApi.cs
+++ InventarWorkerCommon/Models/Settings/ClientApi.cs
@@ -5,6 +5,7 @@ namespace InventarWorkerCommon.Models.Settings;
 /// </summary>
 public class ClientApi
 {
+
     /// <summary>
     /// Gets the fully qualified domain name (FQDN) of the client API.
     /// </summary>
@@ -14,4 +15,17 @@ public class ClientApi
     /// Gets the port of the client API.
     /// </summary>
     public string ClientApiPort { get; set; }
+
+    /// <summary>
+    /// Gets the full URL of the client API, constructed using the FQDN and port information.
+    /// </summary>
+    public string ClientApiUrl
+    {
+        get
+        {
+            var host = string.IsNullOrWhiteSpace(ClientApiFqdn) ? "localhost" : ClientApiFqdn;
+            var port = string.IsNullOrWhiteSpace(ClientApiPort) ? "80" : ClientApiPort;
+            return $"http://{host}:{port}/api";
+        }
+    }
 }
