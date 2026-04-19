44cb735e60dc0bd369693abb5d1a52d2baf6bad6
Thorsten Hindermann <hindermath@googlemail.com>
2026-04-19 22:57:56 +0200

    Patch generated DocFX theme script

--- .github/scripts/postprocess-docfx-site.sh
+++ .github/scripts/postprocess-docfx-site.sh
@@ -14,3 +14,8 @@ while IFS= read -r -d '' html_file; do
   perl -0pi -e 's#<img id="logo" class="svg" src="([^"]+)" alt="InventarWorkerService">#<img id="logo" class="svg" src="$1" alt="" aria-hidden="true">#g' "$html_file"
   perl -0pi -e 's#(<a\b[^>]*\bclass="[^"]*\bdropdown-toggle\b[^"]*"[^>]*?)\saria-expanded="false"([^>]*>)#$1$2#g' "$html_file"
 done < <(find "$site_root" -type f -name '*.html' -print0)
+
+if [[ -f "$site_root/public/docfx.min.js" ]]; then
+  perl -0pi -e 's!<a title='\''\$\{L\("changeTheme"\)\}'\'' class='\''btn border-0 dropdown-toggle'\'' data-bs-toggle='\''dropdown'\'' aria-expanded='\''false'\''>!<a title='\''\$\{L\("changeTheme"\)\}'\'' class='\''btn border-0 dropdown-toggle'\'' data-bs-toggle='\''dropdown'\''>!g' "$site_root/public/docfx.min.js"
+  perl -0pi -e 's!<a class='\''nav-link dropdown-toggle \$\{s\}'\'' href='\''#'\'' role='\''button'\'' data-bs-toggle='\''dropdown'\'' aria-expanded='\''false'\''>!<a class='\''nav-link dropdown-toggle \$\{s\}'\'' href='\''#'\'' role='\''button'\'' data-bs-toggle='\''dropdown'\''>!g' "$site_root/public/docfx.min.js"
+fi
